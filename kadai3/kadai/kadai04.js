e435030f31926283949da632af5e6134edb63398
楢館真市 <naraaa.tx512zz@gmail.com>
2020-05-21 08:05:37 +0000

    jQuery 03 課題4を修正いたしました。

--- kadai3/kadai/kadai04.js
+++ kadai3/kadai/kadai04.js
@@ -2,5 +2,8 @@
   $(function() {
     // ここに処理を書きます
     $('#date_picker').flatpickr({"locale": "ja"});
+    $('#date_picker_button').on("click",function(){
+      $("#date_picker").flatpickr().open();
+    });
   });
 })(jQuery, window);
